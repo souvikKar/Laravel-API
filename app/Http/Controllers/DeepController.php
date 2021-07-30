f828a1e30d8a8eb94a22c13a29a970cbade012fe
souvik <souvikkar0101@gmail.com>
2021-07-30 22:12:49 +0530

    Laravel API

---
+++ app/Http/Controllers/DeepController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tab;
+
+class DeepController extends Controller
+{
+    //
+    public function present(Request $req){
+     //return Tab::all(); for view perpous from Database
+
+     $tab = new Tab;
+     $tab->name = $req->name;
+     $tab->title = $req->title;
+     $tab->address = $req->address;
+
+     $result = $tab->save();
+
+     if($result){
+         return ["Result"=>"Data has been Saved"];
+     } else {
+        return ["Result"=>"Operation Failed"];
+     }
+}
+}
